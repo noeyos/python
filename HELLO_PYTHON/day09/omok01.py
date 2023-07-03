f54dfe966f2ff10f69b0fbf67413ef87bd900c08
noeyos <leesoyeon7038@gmail.com>
2023-07-03 09:36:40 +0900

    no message

--- HELLO_PYTHON/day09/omok01.py
+++ HELLO_PYTHON/day09/omok01.py
@@ -1,27 +1,22 @@
-from PyQt5 import QtWidgets, uic
 import sys
-from PyQt5.QtWidgets import QLabel
-from PyQt5.Qt import QPixmap
-form_window = uic.loadUiType("omok01.ui")[0]
+from PyQt5 import uic, QtGui
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
-class UiMainWindow(QtWidgets.QMainWindow, form_window):
+form_class = uic.loadUiType("omock01.ui")[0]
+
+
+class WindowClass(QMainWindow, form_class):
+
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.show()
-        
-    def initUI(self):
-        self.lbl = QLabel(self)
-        self.lbl.resize(100,100)
-        pixmap = QPixmap("1.png")
-        self.lbl.setPixmap(QPixmap(pixmap))
-        
-        
-
-        
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    main_window = UiMainWindow()
-    sys.exit(app.exec_())
-
+        self.pushButton.clicked.connect(self.myclick)
     
+    def myclick(self):
+        self.pushButton.setIcon(QtGui.QIcon("1.png"))
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = WindowClass()
+    myWindow.show()
+    app.exec_()
